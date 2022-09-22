d86b90841dca41965d4a27694668a7def214b223
Williams392 <williamsvalle392@gmail.com>
2022-09-22 02:58:42 -0500

    goo

---
+++ 2_Tarea/Tarea_2.js
@@ -0,0 +1,15 @@
+
+//    -- Tare 2 --
+
+const lista = [
+    "Williams Valle Dextre",
+    19,
+    true,
+    new Date(2003, 05, 23),
+    {
+        Titulo: "La ciudad y los perros",
+        Autor: "Mario Vargas Llosa",
+        Fecha: new Date(1963),	
+        url: "https://www.ingenieria.unam.mx/dcsyhfi/material_didactico/Literatura_Hispanoamericana_Contemporanea/Autores_V/VARGAS/PERROS.pdf",
+    },
+];
