9aab1b4bd24e18a53a10b3c49ab07d16a6cc1b89
Williams392 <williamsvalle392@gmail.com>
2022-10-04 21:56:42 -0500

    Tarea_5.1

---
+++ 5_Tarea/Tarea_5.js
@@ -0,0 +1,17 @@
+// Una variable que contenga tu altura en centímetros (entero)
+const altura_cm = 185
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+const altura_m = 1.85
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+const peso_kg = 95.2
+
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+const altura_red = Math.ceil(altura_m)
+
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+const peso_red = Math.floor(peso_kg)
+
+//  Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
